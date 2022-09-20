e1fc823a1d99fb5b543594a71d7d73a6d7e8c0c5
danieldificil <daniel.araujo@ideabox-it.com>
2022-09-20 02:53:26 -0300

    dia-2

---
+++ 02-manipulacao-de-objetos/telefone.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Daniel",
+    idade: "28",
+    cpf: "111.111.117-77",
+    email: "daniel777@hotsete.com",
+    fones: ["4002-8922", "7777-7777"]
+}
+cliente.fones.forEach((fone) => {console.log(fone)} )
